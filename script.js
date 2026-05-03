f482b81c5e0ca27009e18aa45811ffad8ec80f09
mozawafm <54464900+mozawafm@users.noreply.github.com>
2026-05-03 23:20:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,20 +18,29 @@ window.onload = function() {
     if (visits == 1) {
         messages = [
             "ты уже видела это видео",
-            "ты просто не помнишь",
-            "это часть процесса"
+            "это часть процесса",
+            "всё идёт правильно"
         ];
-    } else if (visits == 2) {
+    } 
+    else if (visits == 2) {
         messages = [
             "ты согласилась",
-            "мы отправили тебе ссылку",
-            "ты перешла"
+            "мы отправили тебе адрес",
+            "ты его видела"
         ];
-    } else {
+    } 
+    else if (visits == 3) {
         messages = [
-            "ты придешь",
-            "адрес уже у тебя",
-            "ты нажмешь play"
+            "ты идёшь",
+            "никого не будет",
+            "ты нажмёшь play"
+        ];
+    } 
+    else {
+        messages = [
+            "зачем ты снова проверяешь",
+            "ты уже там была",
+            "ты уже нажимала"
         ];
     }
 
@@ -59,9 +68,11 @@ window.onload = function() {
 
             let r = Math.random();
 
-            if (r > 0.6) {
+            if (r > 0.65) {
                 window.location.href = "observe.html";
-            } else if (r > 0.3) {
+            } else if (r > 0.4) {
+                window.location.href = "address.html";
+            } else if (r > 0.2) {
                 window.location.href = "invite.html";
             } else {
                 window.location.href = "time.html";
