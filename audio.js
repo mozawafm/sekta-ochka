890afafc4c6b8375f855e00edca2a6625831f424
mozawafm <54464900+mozawafm@users.noreply.github.com>
2026-05-24 15:08:24 +0500

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,31 +1,50 @@
-if(!localStorage.getItem("musicStarted")){
+let audio = document.getElementById("globalAudio");
 
-    const audio = new Audio("audio/ambient.mp3");
+if(!audio){
+
+    audio = document.createElement("audio");
+
+    audio.id = "globalAudio";
+
+    audio.src = "audio/ambient.mp3";
 
     audio.loop = true;
 
     audio.volume = 0.18;
 
-    audio.play();
-
-    window.globalAudio = audio;
-
-    localStorage.setItem("musicStarted","true");
-
-}else{
-
-    if(!window.globalAudio){
-
-        const audio = new Audio("audio/ambient.mp3");
-
-        audio.loop = true;
-
-        audio.volume = 0.18;
-
-        audio.play();
-
-        window.globalAudio = audio;
-
-    }
+    document.body.appendChild(audio);
 
 }
+
+// СОХРАНЯЕМ ВРЕМЯ
+
+const savedTime = localStorage.getItem("audioTime");
+
+if(savedTime){
+
+    audio.currentTime = savedTime;
+
+}
+
+// АВТОСОХРАНЕНИЕ ПОЗИЦИИ
+
+setInterval(()=>{
+
+    localStorage.setItem(
+        "audioTime",
+        audio.currentTime
+    );
+
+},1000);
+
+// ЗАПУСК ПОСЛЕ КЛИКА
+
+function startAudio(){
+
+    audio.play();
+
+    document.removeEventListener("click", startAudio);
+
+}
+
+document.addEventListener("click", startAudio);
